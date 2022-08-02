7ebbcd054a1fe3f4bce6992502284174eab3cbb8
Alexander Ferrena <apf2139@columbia.edu>
2022-08-02 16:32:25 -0400

    add gridextra to dependencies

--- R/survival.R
+++ R/survival.R
@@ -2,8 +2,8 @@
 
 
 # to do items
-# consider saving things in the first survival function, just plotting in later funtions?
-# add in all later functions: summary plot, table, and forestplots
+# finish adding saving parts of the functions to the end of this
+# may need to split saving for continuous vs categorical
 
 # also, in deplots.r, make sure to check heatmapplot:
 # added suport for sig vs nonsig genes --> test it
@@ -20,6 +20,7 @@
 #' @param multivarnames - a character vector. The colnames of clinvardf to include in multivariable modelling, if desired. If empty, does not perform multivar modelling.
 #' @param timevarname - a string, the colname of the column in clinvardf with time to event information.
 #' @param statusvarname - a string, the colname of the column in clinvardf with event information. The event column should be a character vector and should have the word "Censored" (capital C); ie alive/dead should be "Censored" and "Dead", event should be "Censored" and "Event".
+#' @param outdir - a string, the path to write outputs to; defaults to './survival'
 #'
 #' @return A nested list object: highest level, categorical vs continuous; below that, lists containing plots, models, and data; plotlists will contain kaplan-meier plots; models will include cox regresion models; datalists contain the data DFs, which is important for reproducibility and model diagnostics including proportional hazards testing.
 #' @export
@@ -30,12 +31,15 @@ survival <- function(testvardf,
                      vartypes,
                      multivarnames,
                      timevarname,
-                     statusvarname){
+                     statusvarname,
+                     outdir){
 
-  #if( missing(vartypes) ) {lfclimit <- 15}
+
 
 
   message("\nInitiating analysis")
+
+  if( missing(outdir) ) {outdir <- './survival'; message('\nNo outdir provided, defaulting to write outs to "./survival"')}
 
   ### try to get continuous vs cat test vars ###
   # if vartpyes is provided (not missing), use that
@@ -81,146 +85,6 @@ survival <- function(testvardf,
 
 
 
-
-
-
-
-  #### categorical vars ####
-
-  if( length(catnames) > 0 ){
-
-    message("\nBeginning analysis for categorical variables...")
-
-
-    #get varnames
-    scorenames <- names(catvars)
-
-    # prep progress bar
-    total = ncol(catvars)
-    pb <- txtProgressBar(min = 0, max = total, style = 3)
-
-
-    #prep output lists
-
-    #outs from loop: plots, models, data
-    plotlist <- list()
-    modellist <- list()
-    datalist <- list()
-
-    # this list will wrap the cat var lists, if there are cat vars; else, NULL.
-    catvarout <- list()
-
-    for(scoreidx in 1:length(scorenames)) {
-
-      #get the score name
-      scorename <- colnames(catvars)[scoreidx]
-
-      #get the score valeues, numeric continuopus
-      scorevec <- catvars[,scorename]
-
-      # change name to have N
-      scorevec <- plyr::mapvalues(scorevec, from = levels(scorevec),
-                                  to = paste0(levels(scorevec), '\nN=', table(scorevec)[levels(scorevec)] ) )
-
-
-
-      datadf <- data.frame(var = scorevec,
-                           surv = clinvardf[,timevarname],
-                           status = clinvardf[,statusvarname])
-
-
-      ### code status as numeric ###
-      # EVENT = 1, CENSORED = 0
-      datadf$status_code <- ifelse(datadf$status == 'Censored', yes=0, no = 1)
-
-
-
-      fit <- surv_fit(Surv(surv, status_code) ~ var, data = datadf)
-
-      gcat <- ggsurvplot(fit,
-                         conf.int = F,
-                         pval = TRUE,
-                         pval.method = T,
-                         linetype = "strata",
-                         test.for.trend = F,
-                         title=scorename,
-                         ggtheme = theme_classic2())
-
-      #test for trend, but only if >2 vars...
-      if( length(levels(datadf$var)) > 2 )  {
-
-
-
-        gtft <- ggsurvplot(fit,
-                           conf.int = F,
-                           pval = TRUE,
-                           pval.method = T,
-                           linetype = "strata",
-                           test.for.trend = T,
-                           title=scorename,
-                           ggtheme = theme_classic2())
-
-        gtft <- gtft$plot
-
-      }
-
-      gcat <- gcat$plot
-
-      if( length(levels(datadf$var)) > 2 )  {
-        plots <- list(gcat, gtft)
-
-      } else{
-        plots <- gcat
-      }
-
-      ### cox modelling ###
-      #dich model, will match plot
-      model <- coxph(Surv(surv, status_code) ~ var, data = datadf)
-
-
-      #multivar cox test
-      if( !missing(multivarnames) ){
-
-        #prep for multivar
-        datadf <- cbind(datadf, clinvardf)
-
-        # run it
-        multivar <- coxph(as.formula(
-          paste0('Surv(surv, status_code) ~ ',
-                 paste(multivarnames, collapse = ' + '))
-        ),
-        data=datadf)
-
-      } else{multivar <- NULL}
-
-
-
-      #bind all outputs
-      # models
-      models <- list(model = model,
-                     multivar = multivar
-      )
-
-      # plots
-      # plots <- list(gdich, gterc, distplots)
-      plots <- plots
-
-      #save outs
-      plotlist[[scorename]] <- plots
-      modellist[[scorename]] <- models
-      datalist[[scorename]] <- datadf
-
-
-      setTxtProgressBar(pb, scoreidx)
-
-    } # end cat var loop.
-
-
-    catvarout <- list(plotlist = plotlist,
-                      modellist = modellist,
-                      datalist = datalist)
-
-  } else{catvarout <- NULL} #end cat var if statement.
 
 
 
@@ -395,14 +259,323 @@ survival <- function(testvardf,
 
 
     contvarout <- list(plotlist = plotlist,
-                      modellist = modellist,
-                      datalist = datalist)
+                       modellist = modellist,
+                       datalist = datalist)
 
 
   } else {contvarout <- NULL} # end cont var if statement.
 
-  outlist <- list(categorical = catvarout,
-                  continuous = contvarout)
+
+
+
+
+
+
+
+
+
+
+
+  #### categorical vars ####
+
+  if( length(catnames) > 0 ){
+
+    message("\nBeginning analysis for categorical variables...")
+
+
+    #get varnames
+    scorenames <- names(catvars)
+
+    # prep progress bar
+    total = ncol(catvars)
+    pb <- txtProgressBar(min = 0, max = total, style = 3)
+
+
+    #prep output lists
+
+    #outs from loop: plots, models, data
+    plotlist <- list()
+    modellist <- list()
+    datalist <- list()
+
+    # this list will wrap the cat var lists, if there are cat vars; else, NULL.
+    catvarout <- list()
+
+    for(scoreidx in 1:length(scorenames)) {
+
+      #get the score name
+      scorename <- colnames(catvars)[scoreidx]
+
+      #get the score valeues, numeric continuopus
+      scorevec <- catvars[,scorename]
+
+      # change name to have N
+      scorevec <- plyr::mapvalues(scorevec, from = levels(scorevec),
+                                  to = paste0(levels(scorevec), '\nN=', table(scorevec)[levels(scorevec)] ) )
+
+
+
+      datadf <- data.frame(var = scorevec,
+                           surv = clinvardf[,timevarname],
+                           status = clinvardf[,statusvarname])
+
+
+      ### code status as numeric ###
+      # EVENT = 1, CENSORED = 0
+      datadf$status_code <- ifelse(datadf$status == 'Censored', yes=0, no = 1)
+
+
+
+      fit <- surv_fit(Surv(surv, status_code) ~ var, data = datadf)
+
+      gcat <- ggsurvplot(fit,
+                         conf.int = F,
+                         pval = TRUE,
+                         pval.method = T,
+                         linetype = "strata",
+                         test.for.trend = F,
+                         title=scorename,
+                         ggtheme = theme_classic2())
+
+      #test for trend, but only if >2 vars...
+      if( length(levels(datadf$var)) > 2 )  {
+
+
+
+        gtft <- ggsurvplot(fit,
+                           conf.int = F,
+                           pval = TRUE,
+                           pval.method = T,
+                           linetype = "strata",
+                           test.for.trend = T,
+                           title=scorename,
+                           ggtheme = theme_classic2())
+
+        gtft <- gtft$plot
+
+      }
+
+      gcat <- gcat$plot
+
+      if( length(levels(datadf$var)) > 2 )  {
+        plots <- list(gcat, gtft)
+
+      } else{
+        plots <- gcat
+      }
+
+      ### cox modelling ###
+      #dich model, will match plot
+      model <- coxph(Surv(surv, status_code) ~ var, data = datadf)
+
+
+      #multivar cox test
+      if( !missing(multivarnames) ){
+
+        #prep for multivar
+        datadf <- cbind(datadf, clinvardf)
+
+        # run it
+        multivar <- coxph(as.formula(
+          paste0('Surv(surv, status_code) ~ ',
+                 paste(multivarnames, collapse = ' + '))
+        ),
+        data=datadf)
+
+      } else{multivar <- NULL}
+
+
+
+      #bind all outputs
+      # models
+      models <- list(model = model,
+                     multivar = multivar
+      )
+
+      # plots
+      # plots <- list(gdich, gterc, distplots)
+      plots <- plots
+
+      #save outs
+      plotlist[[scorename]] <- plots
+      modellist[[scorename]] <- models
+      datalist[[scorename]] <- datadf
+
+
+      setTxtProgressBar(pb, scoreidx)
+
+    } # end cat var loop.
+
+
+    catvarout <- list(plotlist = plotlist,
+                      modellist = modellist,
+                      datalist = datalist)
+
+  } else{catvarout <- NULL} #end cat var if statement.
+
+
+
+
+
+
+
+
+
+
+
+
+  ##### WRITE OUTS, SAVE PLOTS ETC #####
+
+  outlist <- list(continuous = contvarout,
+                  categorical = catvarout)
+
+  message('\nPreparing to save outputs to: "', outdir,'"')
+  dir.create(outdir)
+
+
+
+  ### save continuous vars ###
+
+  if(is.null( outlist[[1]]) ){
+
+
+    message('\nSaving continuous variable outputs...')
+
+    #get outputs
+    plotlist <- outlist[[1]][[1]]
+    modellist <- outlist[[1]][[2]]
+    datalist <- outlist[[1]][[3]]
+
+
+
+    sigpways <- names(plotlist)
+
+    sigresdir <- paste0(outdir, '/continuousvars/')
+    dir.create(sigresdir)
+
+    total = length(sigpways)
+    pb <- txtProgressBar(min = 0, max = total, style = 3)
+
+
+    #for significant results, make outputs
+    pdfplotlist <- list()
+    for(scoreidx in 1:length(sigpways) ){
+
+      score <- sigpways[scoreidx]
+      #message(score)
+
+      #get models
+      models <- modellist[[score]]
+      datadf <- datalist[[score]] #load data into env or else, the diagnostics break
+
+      #get plots
+      plots <- plotlist[[score]]
+
+
+      #get the coefficients, hazard ratios, confidence intervals, and P values
+      summarytable <- bind_rows( lapply(models, function(x){as.data.frame( summary(x)$coefficients ) }) )
+      summarytable <- bind_cols(summarytable,
+                                bind_rows( lapply(models, function(x){as.data.frame( summary(x)$conf.int ) }) )[,3:4] )
+
+
+
+
+
+      #only plot if significant...
+      # ignore met
+      # pvals <- summarytable$`Pr(>|z|)`[-(nrow(summarytable))]
+      # if( !any(pvals < 0.10) ){
+      #   next
+      # }
+      # this is defined by sigpways, not necessary
+
+      #rename the continuous
+      rownames <- rownames(summarytable)
+      rownames[grepl('^continuous', rownames)] <- c('Univariable\nContinuous', 'Multivariable\nContinuous')
+      rownames(summarytable) <- rownames
+
+      #put conf int next to coef
+      summarytable <- summarytable[,c(1,6,7,2,3,4,5)]
+
+      #hr, lower/upperCI, P, and sig only...
+      summarytable <- summarytable[,c(4,2,3,7)]
+
+
+      #add significance marks
+      summarytable$significance <- ''
+      summarytable[summarytable$`Pr(>|z|)` < 0.1, "significance"] <- '.'
+      summarytable[summarytable$`Pr(>|z|)` < 0.05, "significance"] <- '*'
+      summarytable[summarytable$`Pr(>|z|)` < 0.01, "significance"] <- '**'
+      summarytable[summarytable$`Pr(>|z|)` < 0.001, "significance"] <- '***'
+
+      #round table
+      summarytable[,-ncol(summarytable)] <- round( summarytable[,-ncol(summarytable)],3 )
+
+      # plot it all together
+      updatedplot <- (plots[[1]] + plots[[2]]) / ( gridExtra::tableGrob(summarytable)) + patchwork::plot_annotation(score) +
+        plot_layout(heights = c(1,2))
+
+
+      pdfplotlist[[score]] <- updatedplot
+
+
+
+      #save all outputs...
+      resultdir <- paste0(sigresdir, '/', score)
+      dir.create(resultdir)
+
+
+      # saveRDS( file = paste0(resultdir, '/models.rds'), models  )
+
+
+      #check model assumptions...
+      modeldiagnosticsfile <- paste0(resultdir, '/modeldiagnostics.pdf')
+      pdf(modeldiagnosticsfile, height = 7, width = 7)
+      for(model in models){
+
+        print( ggcoxzph( cox.zph(model) ) )
+
+      }
+      dev.off()
+
+
+      #save plots
+
+
+      gdich <- plots[[1]]
+      gterc <- plots[[2]]
+      distplots <- plots[[3]]
+      suppressMessages(ggsave( paste0(resultdir, '/dist.jpg'), distplots , height = 7, width = 7))
+
+      ggsave( paste0(resultdir, '/gdich.jpg'), gdich , height = 5, width = 5)
+      ggsave( paste0(resultdir, '/gterc.jpg'), gterc , height = 5, width = 5)
+
+
+      pdf(paste0(resultdir, '/summaryplot.pdf'), height = 10, width = 9)
+      updatedplot
+      dev.off()
+
+      summarytable <- cbind(rownames(summarytable), summarytable)
+
+      write.csv(paste0(resultdir, '/modeltable.csv'), x=summarytable, row.names = F)
+
+
+      ### saveas much as possible just in case ###
+      list_to_save <- list(datadf = datadf, plots = plots, models = models)
+      saveRDS(object = list_to_save, file = paste0(resultdir, '/objects.rds') )
+
+      setTxtProgressBar(pb, scoreidx)
+
+
+  }
+
+
+
+
+  }
+
+
+
 
   return(outlist)
 
